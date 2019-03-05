7ff5e6922273640bcaa47d8857d63911725575e8
rachelkathleen <rachel.hawa@gmail.com>
2019-03-05 15:45:05 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+     "My name is Rachel"
+  end
+
+  get '/hometown' do
+    "My hometown is Brooklyn"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Howl'"
+  end
 end
